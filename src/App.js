f02fa6ddfd96d8ab7dc51c707d3e199a7226d3b9
Alima <alimausmani21@navgurukul>
2023-10-16 17:11:05 +0530

    UI Done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="box1">
+      <img src="https://media.istockphoto.com/id/1282162847/photo/close-up-portrait-of-beautifully-looking-young-blonde-haired-woman-dressed-in-a-delicate.jpg?s=612x612&w=0&k=20&c=cSvsg2MzdIhFGM-JXEB2iI6yV0GsRyBszfSbvqjT8HA=" alt="Alima Image" />
+      <h1>Riya Gautom</h1>
+      <h5>Stranger</h5>
+      <button className='remove'>Remove</button>
+      <button className='add'>Add Friend</button>
+     
+    </div>
+  );
+}
+
+export default App; 
