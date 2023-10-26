d047f4e3762ea1d518c19c1738b61967d8d4368f
Alima <alimausmani21@navgurukul>
2023-10-26 10:56:38 +0530

    Button is Working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Box() {
+  const [addfriend, setAddfriend] = useState("Stranger");
+  const [remove, setRemove] = useState("");
+  
   return (
     <div className="box1">
       <img src="https://media.istockphoto.com/id/1282162847/photo/close-up-portrait-of-beautifully-looking-young-blonde-haired-woman-dressed-in-a-delicate.jpg?s=612x612&w=0&k=20&c=cSvsg2MzdIhFGM-JXEB2iI6yV0GsRyBszfSbvqjT8HA=" alt="Alima Image" />
       <h1>Riya Gautom</h1>
-      <h5>Stranger</h5>
-      <button className='remove'>Remove</button>
-      <button className='add'>Add Friend</button>
-     
+      <h5 style={{ color: addfriend === "Friend" ? "green" : "red" }}>{addfriend}</h5>
+      <button className='remove' onClick={()=>setAddfriend("Stranger")}>Remove</button>
+      <button className='add' onClick={()=>setAddfriend("Friend")}>Add Friend</button>
     </div>
   );
 }
 
-export default App; 
+export default Box;
